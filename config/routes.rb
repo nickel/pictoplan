ebe6ef6e9dd0a8825674a9c105bca1c79e98b37a
Juan Gallego IV <juan@gallegoiv.com>
2024-03-27 12:21:01 +0100

    Styling login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :accounts
+  devise_for :accounts, controllers: {
+    sessions: "accounts/sessions",
+    registrations: "accounts/registrations",
+    passwords: "accounts/passwords"
+  }
 
   get "up" => "rails/health#show", as: :rails_health_check
 
