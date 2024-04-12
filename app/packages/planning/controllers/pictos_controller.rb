50d8e5aabde4062a2a049dde315572d6e572ed3b
Juan Gallego IV <juan@gallegoiv.com>
2024-04-12 20:46:13 +0200

    Mad about Security🔐 - Added an admin check in pictos_controller.rb 🕵️‍♀️ - Removed pictos link from _header.html.erb for non-admins 👀
    Why? Because we don't want the pesky non-admins messing with the precious pictos now, do we? 😀

--- app/packages/planning/controllers/pictos_controller.rb
+++ app/packages/planning/controllers/pictos_controller.rb
@@ -2,6 +2,7 @@
 
 class PictosController < ApplicationController
   before_action :authenticate_account!
+  before_action :ensure_admin!
 
   def index
     @pictos = Picto::FindAll
@@ -24,5 +25,11 @@ class PictosController < ApplicationController
       .call(picto_id: params[:id])
 
     redirect_to pictos_path(q: params[:q], only_disabled: params[:only_disabled])
+  end
+
+  private
+
+  def ensure_admin!
+    redirect_to root_path unless current_account.admin?
   end
 end
